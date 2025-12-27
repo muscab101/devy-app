023cb6d3f553ff90ac3819402aec48f6ebf4e9e8
muscab101 <muscabmusa27@gmail.com>
2025-12-27 23:33:35 +0000

    2

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
     <div className="flex min-h-screen flex-col">
       {/* Header */}
       <header className="border-b">
-        <div className="container flex h-16 items-center justify-center gap-15 ">
+        <div className="container flex h-16 items-center justify-between px-4">
           <Link href="/" className="flex items-center gap-2">
             <Image src="/images/logo.png" alt="Devy Logo" width={120} height={40} className="h-8 w-auto" priority />
           </Link>
